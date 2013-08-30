0f0a324cec055ed33ed036e593e237c5fafc324b
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-08-30 16:50:39 +0900

    add validation of k random permutation array

--- lib/Digest/bBitMinHash.pm
+++ lib/Digest/bBitMinHash.pm
@@ -11,15 +11,22 @@ use Digest::MurmurHash3;
 our $VERSION = "0.0.0_01";
 
 sub new {
-    my ($class, $b, $k) = @_;
+    my ($class, $b, $k, $seed_arr_ref) = @_;
+    $b = 1 unless (($b) && ($b > 0));
+    $k = 128 unless (($k) && ($k > 0));
+    $seed_arr_ref = Digest::bBitMinHash->init_seeds($k) unless ((defined $seed_arr_ref) && (ref $seed_arr_ref eq "Array") && (($#$seed_arr_ref + 1) >= $k));
     my %hash = (
         'b' => $b,
         'k' => $k,
+        'seed' => $seed_arr_ref,
     );
-    $b = 1 unless (($b) && ($b > 0));
-    $k = 128 unless (($k) && ($k > 0));
     bless \%hash, $class;
 }
+
+
+
+
+
 
 1;
 
